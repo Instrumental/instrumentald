12f2de9a4594c5800dacfef56819a3f51b8d14a0
Chris <christopher@fastestforward.com>
2015-04-29 12:52:11 -0400

    remove unnecessary cat|grep uses in favor of proc fs reading

--- lib/instrumental_tools/system_inspector/linux.rb
+++ lib/instrumental_tools/system_inspector/linux.rb
@@ -7,9 +7,33 @@ class SystemInspector
       output
     end
 
+    def self.cpu_file
+      "/proc/stat"
+    end
+
+    def self.load_file
+      "/proc/loadavg"
+    end
+
+    def self.disk_file
+      "/proc/diskstats"
+    end
+
+    def self.mount_file
+      "/proc/mounts"
+    end
+
+    def self.open_files_file
+      "/proc/sys/fs/file-nr"
+    end
+
+    def self.memory_file
+      "/proc/meminfo"
+    end
+
     def self.cpu
       categories = [:user, :nice, :system, :idle, :iowait]
-      values     = `cat /proc/stat | grep cpu[^0-9]`.chomp.split.slice(1, 5).map { |v| v.to_f }
+      values     = File.read(cpu_file).lines.grep(/\Acpu[^0-9]/).slice(1, 5).map { |v| v.to_f }
       SystemInspector.memory.store(:cpu_values, values.dup)
       if previous_values = SystemInspector.memory.retrieve(:cpu_values)
         index = -1
@@ -29,7 +53,7 @@ class SystemInspector
     end
 
     def self.loadavg
-      min_1, min_5, min_15 = `cat /proc/loadavg`.split
+      min_1, min_5, min_15 = File.read(load_file).split
       {
         'load.1min'  => min_1.to_f,
         'load.5min'  => min_5.to_f,
@@ -39,34 +63,41 @@ class SystemInspector
 
     def self.load_memory
       output = { :gauges => {} }
-      if SystemInspector.command_present?('free', 'memory')
+      if File.exists?(memory_file)
         output[:gauges].merge!(memory)
-      end
-      if SystemInspector.command_present?('free', 'swap')
-        output[:gauges].merge!(swap)
       end
       output
     end
 
     def self.memory
-      _, total, used, free, shared, buffers, cached = `free -k -o | grep Mem`.chomp.split
-      {
-        'memory.used_mb'      => used.to_f / 1024,
-        'memory.free_mb'      => free.to_f / 1024,
-        'memory.buffers_mb'   => buffers.to_f / 1024,
-        'memory.cached_mb'    => cached.to_f / 1024,
-        'memory.free_percent' => (free.to_f / total.to_f) * 100
-      }
-    end
+      memory_stats = Hash[File.read(memory_file).lines.map { |line| line.chomp.strip.split(/:\s+/) }.reject { |l| l.size != 2 } ]
+      total        = memory_stats["MemTotal"].to_f
+      free         = memory_stats["MemFree"].to_f
+      used         = free - total
+      buffers      = memory_stats["Buffers"].to_f
+      cached       = memory_stats["Cached"].to_f
+      swaptotal    = memory_stats["SwapTotal"].to_f
+      swapfree     = memory_stats["SwapFree"].to_f
+      swapused     = swapfree - swaptotal
 
-    def self.swap
-      _, total, used, free = `free -k -o | grep Swap`.chomp.split
-      return {} if total.to_i == 0
-      {
-        'swap.used_mb'      => used.to_f / 1024,
-        'swap.free_mb'      => free.to_f / 1024,
-        'swap.free_percent' => (free.to_f / total.to_f) * 100
+      stats_to_record = {
+        'memory.used_mb'      => used / 1024,
+        'memory.free_mb'      => free / 1024,
+        'memory.buffers_mb'   => buffers / 1024,
+        'memory.cached_mb'    => cached / 1024,
+        'memory.free_percent' => (free / total) * 100,
+
       }
+
+      if swaptotal > 0
+        stats_to_record.merge!({
+                                 'swap.used_mb'        => swapused / 1024,
+                                 'swap.free_mb'        => swapfree / 1024,
+                                 'swap.free_percent'   => (swapfree / swaptotal) * 100
+                               })
+      end
+
+      stats_to_record
     end
 
     def self.load_disks
@@ -74,7 +105,7 @@ class SystemInspector
       if SystemInspector.command_present?('df', 'disk storage')
         output[:gauges].merge!(disk_storage)
       end
-      if SystemInspector.command_present?('mount', 'disk IO')
+      if File.exists?(mount_file) && File.exists?(disk_file)
         output[:gauges].merge!(disk_io)
       end
       output
@@ -104,10 +135,9 @@ class SystemInspector
 
     def self.disk_io
       output          = {}
-      mounted_devices = `mount`.lines.grep(/^\/dev\/(\w+)/) { $1 }
-      diskstats_lines = `cat /proc/diskstats`.lines.grep(/#{mounted_devices.join('|')}/)
-      entries         = diskstats_lines.map do |line|
-                          values              = line.chomp.split
+      mounted_devices = File.read(mount_file).lines.grep(/\A\/dev\/(\w+)/) { File.realpath($1) }
+      diskstats_lines = File.read(disk_file).lines.map(&:split).select { |values| mounted_devices.include?(values.first) }
+      entries         = diskstats_lines.map do |values|
                           entry               = {}
                           entry[:time]        = Time.now
                           entry[:device]      = values[2]
@@ -127,14 +157,14 @@ class SystemInspector
 
     def self.load_filesystem
       output = { :gauges => {} }
-      if SystemInspector.command_present?('sysctl', 'filesystem')
+      if File.exists?(open_files_file)
         output[:gauges].merge!(filesystem)
       end
       output
     end
 
     def self.filesystem
-      allocated, unused, max = `sysctl fs.file-nr`.split[-3..-1].map { |v| v.to_i }
+      allocated, unused, max = File.read(open_files_file).split.map(&:to_i)
       open_files             = allocated - unused
       {
         'filesystem.open_files'         => open_files,
