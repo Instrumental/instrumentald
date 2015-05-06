7aa5758fbae0c572a23e2bfc168eb69dff8d4b7c
Chris <christopher@fastestforward.com>
2015-05-06 14:49:15 -0400

    more simple tests of installation process

--- test/integration/default/serverspec/instrumental_tools_spec.rb
+++ test/integration/default/serverspec/instrumental_tools_spec.rb
@@ -11,3 +11,19 @@ describe service('instrument_server') do
   it { should be_enabled }
   it { should be_running }
 end
+
+describe file('/opt/instrumental-tools/instrument_server.pid') do
+  it { should be_file }
+  it { should be_owned_by('nobody') }
+end
+
+describe file('/etc/instrumental.yml') do
+  it { should be_file }
+  it { should be_owned_by('nobody') }
+end
+
+describe process('ruby') do
+  it         { should be_running }
+  its(:user) { should eq 'nobody' }
+  its(:args) { should match /instrument_server/ }
+end
