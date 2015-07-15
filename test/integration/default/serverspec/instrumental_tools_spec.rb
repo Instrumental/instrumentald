a23df3fc2807c07ed7301ae7f4537162681b3e67
Chris <christopher@fastestforward.com>
2015-07-15 17:36:57 -0400

    remove file existence tests, not necessary w/ service running test

--- test/integration/default/serverspec/instrumental_tools_spec.rb
+++ test/integration/default/serverspec/instrumental_tools_spec.rb
@@ -6,33 +6,21 @@ if RUBY_PLATFORM =~ /(win|mswin|mingw)/i
   set :backend, :cmd
   set :os, :family => 'windows'
   details = {
-    check_executable: false,
     check_owner:      false,
     config:           "c:\\Program Files (x86)\\Instrumental Tools\\etc\\instrumental.yml",
-    executable:       "c:\\Program Files (x86)\\Instrumental Tools\\instrument_server.bat",
     has_pid:          false,
     service_name:     "Instrument Server"
   }
 else
   set :backend, :exec
   details = {
-    check_executable: true,
     check_owner:      true,
     config:           "/etc/instrumental.yml",
-    executable:       "/opt/instrumental-tools/instrument_server",
     has_pid:          true,
     pid_path:         "/opt/instrumental-tools/instrument_server.pid",
     owner:            "nobody",
     service_name:     "instrument_server"
   }
-end
-
-
-describe file(details[:executable]) do
-  it { should be_file }
-  if details[:check_executable]
-    it { should be_executable }
-  end
 end
 
 describe service(details[:service_name]) do
