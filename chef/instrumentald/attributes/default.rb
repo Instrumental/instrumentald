5d695a4f6fd4ab5519ca1006197d6e914228646a
Jason Gladish <jason@expectedbehavior.com>
2016-07-12 17:01:53 -0400

    Updated a few references to /etc/instrumental.toml to /etc/instrumentald.toml, which we use more commonly.
    This should fix an issue with running instrumentald after installing it and editing the config.

--- chef/instrumentald/attributes/default.rb
+++ chef/instrumentald/attributes/default.rb
@@ -14,11 +14,11 @@ default[:instrumental][:enable_scripts]  = false
 
 if node[:platform_family] == "windows"
   default[:instrumental][:destination_dir] = "C:\\Program Files (x86)\\Instrumental Tools"
-  default[:instrumental][:config_file]     = "C:\\Program Files (x86)\\Instrumental Tools\\etc\\instrumental.toml"
+  default[:instrumental][:config_file]     = "C:\\Program Files (x86)\\Instrumental Tools\\etc\\instrumentald.toml"
   default[:instrumental][:script_dir]      = "C:\\Program Files (x86)\\Instrumental Tools\\scripts"
 else
   default[:instrumental][:destination_dir] = "/opt/instrumentald/"
-  default[:instrumental][:config_file]     = "/etc/instrumental.toml"
+  default[:instrumental][:config_file]     = "/etc/instrumentald.toml"
   default[:instrumental][:script_dir]      = "/opt/instrumentald/.scripts"
   default[:instrumental][:pid_file]        = "/opt/instrumentald/instrumentald.pid"
   default[:instrumental][:log_file]        = "/opt/instrumentald/instrumentald.log"
