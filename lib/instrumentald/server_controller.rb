b5429a8c50d072695b9daae392a2666915ca3733
Joel Meador <joel@expectedbehavior.com>
2016-07-20 14:21:10 -0400

    default redis servers shold be empty set

--- lib/instrumentald/server_controller.rb
+++ lib/instrumentald/server_controller.rb
@@ -189,7 +189,7 @@ class ServerController < Pidly::Control
 
   def process_telegraf_config
     instrumental_api_key = configured_api_key
-    redis_servers = config_file['redis']
+    redis_servers = config_file['redis'] || []
     memcached_servers = config_file['memcached'] || []
     postgresql_servers = config_file['postgresql'] || []
 
