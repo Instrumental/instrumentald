8a23755e0af59a805caa31e8f9006285548ac885
mediocretes <nathan@expectedbehavior.com>
2016-08-10 18:03:05 -0400

    add hostname and namespace details to the log size plugin script

--- examples/plugin_scripts/log_size.sh
+++ examples/plugin_scripts/log_size.sh
@@ -1,1 +1,1 @@
-ls -s --block-size=1 /var/log | grep '.*\.log$' | grep -o '.*[^.log]' | awk '{ print $2 " " $1}'
+ls -s --block-size=1 /var/log | grep '.*\.log$' | grep -o '.*[^.log]' | awk '{ print "hostname.log_size." $2 " " $1}' | sed 's/hostname/'"`hostname`"'/g'
